95b3c9ac8b4cc8917fde72562fbf19aa67a724cd
EmTrevDallas <e3520021@student.dcccd.edu>
2026-04-15 15:11:16 -0500

    Delete WebCode directory

--- WebCode/Main.kt
+++
@@ -1,6 +0,0 @@
-package Mainframe.Maincode.Admin
-
-fun main() {
-    startServer()
-    println("Server running on http://localhost:8080")
-}
--- WebCode/Movie.kt
+++
@@ -1,9 +0,0 @@
-package Mainframe.Maincode.Admin
-
-data class Movie(
-    val title: String,
-    val duration: Int,
-    val description: String,
-    val theater: String,
-    val poster: String
-)
