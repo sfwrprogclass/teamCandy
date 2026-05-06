459312c86c1b35be6fe024df24a3b08fbe943dfe
EmTrevDallas <e3520021@student.dcccd.edu>
2026-05-05 22:06:08 -0500

    Delete kotlin directory

--- kotlin/models/Theater.kt
+++
@@ -1,8 +0,0 @@
-package edu.teamcandy.models
-
-data class Theater(
-    // Theater number identifier
-    val number: Int,
-    // List of show times for this theater
-    val showtimeList: MutableList<Showtime> = mutableListOf()
-)
